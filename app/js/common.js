8ce3ce661d755f2192b19cdc57bca06876506e52
Herr-Gunter <kubyshkin1990@mail.ru>
2017-08-07 16:11:14 +0300

    07.08

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,14 @@
 $(function() {
 
-	// Custom JS
+	$(document).ready(function(){
+		$('.owl-carousel').owlCarousel({
+		loop: true,
+		//dots: false,
+		nav: true,
+		smartSpeed: 700,
+		items: 1,
+		navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+		});
+	});
 
 });
